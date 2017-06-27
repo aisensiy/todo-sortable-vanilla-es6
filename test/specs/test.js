80a66f38068f6f641425ebee739c16c4bda6190c
aisensiy <aisensiy@163.com>
2017-06-27 12:49:04 +0800

    test update

--- test/specs/test.js
+++ test/specs/test.js
@@ -3,6 +3,8 @@ var dragAndDrop = require('html-dnd').codeForSelectors;
 module.exports = {
     'Should move first todo to next todo position': function(browser) {
         browser
+            .url(browser.launch_url)
+            .waitForElementVisible('body', 500)
             .execute(dragAndDrop, ['.todo-list li:first-child', '.todo-list li:nth-child(2)'])
             .pause(500)
             .assert.containsText('.todo-list li:nth-child(2)', 'todo 1')
@@ -10,6 +12,8 @@ module.exports = {
     },
     'Should move last todo to first todo position': function(browser) {
         browser
+            .url(browser.launch_url)
+            .waitForElementVisible('body', 500)
             .execute(dragAndDrop, ['.todo-list li:last-child', '.todo-list li:first-child'])
             .pause(500)
             .assert.containsText('.todo-list li:first-child', 'todo 3')
